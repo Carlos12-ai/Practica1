4e5133ef51f5941666bce575fabc18bdc3a16372
Carlos12-ai <carlosticante09@gmail.com>
2025-01-15 10:44:16 -0600

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -12,3 +12,24 @@ def main_menu():
         print("9. Salir")
         
         opcion = input("Seleccione una opcion: ")
+        if opcion == '1':
+            suma_numeros()
+        elif opcion == '2':
+            producto_numeros()
+        elif opcion == '3':
+            division_numeros()
+        elif opcion == '4':
+            calcular_factorial()
+        elif opcion == '5':
+            tablas_multiplicar()
+        elif opcion == '6':
+            cuadrado_cubo()
+        elif opcion == '7':
+            promedio_numeros()
+        elif opcion == '8':
+            valores_max_min()
+        elif opcion == '9':
+            print("Saliendo del programa. Hasta luego")
+            break
+        else:
+            print("Opcion no valida. Intente de nuevo.")
